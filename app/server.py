12a419b720d9feb67d7e2e662ecc55b64a233dca
Diogo Dutra <diogodutra@gmail.com>
2021-03-07 18:36:44 -0500

    Updated FastAI model download link

--- app/server.py
+++ app/server.py
@@ -11,12 +11,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1ntlwwv3Ao3kLJ_VaXgI6Gx3_01HG1Zbz?alt=media&key=AIzaSyA1CbVi3ynikmMs4KXq1xXnHSol27UaQ2U'
+
+export_file_url = 'https://drive.google.com/uc?export=download&id=14fMbTAr5DUmayd1eaKlu2K6_NGPsqtBr'
 export_file_name = 'export.pkl'
 
 Port = int(os.environ.get('PORT', 50000))
 
-classes = ['bulbasaur', 'charmander', 'pikachu']
 path = Path(__file__).parent
 
 app = Starlette()
